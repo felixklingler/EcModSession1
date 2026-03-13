1bfc5e8ff010b23fe7a5f8d306a536542b8d0ecc
felixklingler <felix.kli14@gmail.com>
2026-03-13 12:50:06 +0100

    Added more things

--- point.py
+++ point.py
@@ -21,8 +21,32 @@ class Point: # classes start with capital letters always
         :return: String representation of the point class.
         """
         return f"P<{self.x},{self.y}>" # returns P<x,y>
-
-
+    def __repr__(self):
+        """
+        If the instance is inside of a list, it will be printed nicely
+        :return:
+        """
+        return self.__str__()
+    def distance_origin(self):
+        """
+        Calculates the distance from the origin to the point.
+        :return: float, deistance between origin and point.
+        """
+        return (self.x**2 + self.y**2)**0.5
+    def distance_to(self, point):
+        """
+        Calculates the distance between current point and another point.
+        :param point: the other point to calculate the distance to.
+        :return: float, distance between current point and another point.
+        """
+        return ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) ** 0.5
+    def __lt__(self, other):
+        """
+        Returns true if self is less than other
+        :param other: the other point to compare to.
+        :return: True or False
+        """
+        return self.distance_origin() < other.distance_origin()
 
 
 p1= Point(1,2)
@@ -34,3 +58,18 @@ print(p1.x, p1.y)
 print(p2.x, p2.y)
 print(p3.x, p3.y) # not ideal
 print(p1)
+
+print(f"{p2} distance to origin is {p2.distance_origin()}")
+# print(f"{p3} distance to origin is {p3.distance_origin()}") # makes error, since values are not float
+
+print(f"the distance between {p1} and {p2} is {p1.distance_to(p2)}")
+
+p1 = Point(6,10)
+p2 = Point(6,15)
+p3 = Point(12,5)
+p4 = Point(1,1)
+
+points = [p1, p2, p3, p4, Point(15,6)] # adding a point without adding a line
+print(points)
+points.sort() # sorts by "size", which in this case is distance to origin
+print(points)
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 # Modelling Session 1
 ## Class Examples
 ### Simple Point Class
-
 - initialize the point
 - add __str__ method to print the point
